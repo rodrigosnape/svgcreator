0475e24bc347f095c0d811f2f4b4778017240631
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-05-17 11:00:23 -0300

    Base dos componentes Menu e Canvas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './components/canvas/canvas.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
